eff087a5ce565552ac02bd5e99e9d7428b382a4d
flo4313 <fsmague@gmail.com>
2020-03-02 16:41:03 +0100

    Post page

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,125 +11,125 @@ import Contact from "./Contact";
 import Login from "./Login";
 import Posts from "./Posts";
 import Post from "./Post";
-import {login,logoff,setUser,unSetUser} from '../actions';
+import { login, logoff, setUser, unSetUser } from '../actions';
 class Main extends Component {
 
- 
-  verifLogin(){
+
+  verifLogin() {
     const token = localStorage.token;
-    fetch("http://51.255.175.118:2000/user/verify",{
-      method:"GET",
-      headers:{
-        'Authorization':'Bearer '+token
+    fetch("http://51.255.175.118:2000/user/verify", {
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer ' + token
       }
     })
-    .then(response => response.json())
-    .then(json =>{
-      if(json){
-          if(this.props.isLogged){
-            if(!json.error){
+      .then(response => response.json())
+      .then(json => {
+        if (json) {
+          if (this.props.isLogged) {
+            if (!json.error) {
               this.props.login()
               fetch("http://51.255.175.118:2000/user/" + json.id, {
                 method: "GET",
-                headers:{
-                  'Authorization':'Bearer '+token
+                headers: {
+                  'Authorization': 'Bearer ' + token
                 }
               })
                 .then(res => res.json())
                 .then((data) => {
-                
+
                   this.props.setUser(data[0])
-        
+
                 })
-              }
+            }
           }
-          
-      }
-    })
-    
+
+        }
+      })
+
   }
-  componentDidMount(){
+  componentDidMount() {
     this.verifLogin()
   }
-  logoff(){
-   this.props.logoff();
-    localStorage.setItem("token",null)
+  logoff() {
+    this.props.logoff();
+    localStorage.setItem("token", null)
     window.location.href = '/#/';
     this.props.unSetUser()
   }
 
   render() {
     return (
-      
-        <HashRouter>
-         <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="/#/">
-   
-      <img src="logo1.png"  />
-  
-    </a>
 
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+      <HashRouter>
+        <nav class="navbar " role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="/#/">
 
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item" href="/#/">
-      Home
+              <img src="logo1.png" />
+
+            </a>
+
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+              <a class="navbar-item" href="/#/">
+                Home
       </a>
-      <a class="navbar-item"href="/#/contact" >
-      Contact
+              <a class="navbar-item" href="/#/contact" >
+                Contact
       </a>
-      <a class="navbar-item" href="/#/posts">
-      Posts
+              <a class="navbar-item" href="/#/posts">
+                Posts
       </a>
 
-    
-    </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-        {!this.props.isLogged ? <NavLink to="/login"><a class="button is-primary">
-            <strong>Login</strong>
-          </a></NavLink>: null}
-          
-        {this.props.isLogged ? <button  class="button is-primary" onClick={()=> this.logoff()}>logOff</button>: null}
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
-<section class="section">
-    <div class="container">
-    <Route exact path="/" component={Home}/>
-    <Route path="/stuff" component={Stuff}/>
-    <Route path="/posts" component={Posts}/>
-    <Route path="/post/:id" component={Post}/>
-    <Route path="/login" component={Login}/>
-    <Route path="/contact" component={Contact}/>
-  
-    </div>
-  </section>
+            </div>
 
-        </HashRouter>
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  {!this.props.isLogged ? <NavLink to="/login"><a class="button is-primary">
+                    <strong>Login</strong>
+                  </a></NavLink> : null}
+
+                  {this.props.isLogged ? <button class="button is-primary" onClick={() => this.logoff()}>logOff</button> : null}
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <section class="section">
+          <div class="container">
+            <Route exact path="/" component={Home} />
+            <Route path="/stuff" component={Stuff} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/post/:id" component={Post} />
+            <Route path="/login" component={Login} />
+            <Route path="/contact" component={Contact} />
+
+          </div>
+        </section>
+
+      </HashRouter>
     );
   }
 }
 
 
-const mapStateToProps = (state) =>{
-  return{
-    isLogged : state.isLogged
+const mapStateToProps = (state) => {
+  return {
+    isLogged: state.isLogged
   }
 }
-const mapDispatchToProps = ()=>{
-  return{
+const mapDispatchToProps = () => {
+  return {
     logoff,
     login,
     setUser,
@@ -137,5 +137,5 @@ const mapDispatchToProps = ()=>{
 
   }
 }
- 
-export default connect(mapStateToProps,mapDispatchToProps())(Main);
+
+export default connect(mapStateToProps, mapDispatchToProps())(Main);
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,76 +1,124 @@
 import React, { Component } from "react";
- 
+import Moment from 'react-moment';
 class Posts extends Component {
 
-    constructor(props){
-        super(props)
-        this.state = {
-            posts : null
-        }
-        this.getPosts = this.getPosts.bind(this)
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: null,
+      list: null
     }
-  
-    componentDidMount(){
-     this.getPosts();
-    }
-
-    getPosts(){
-        fetch("http://51.255.175.118:2000/post",{
-            method:"GET"
-        })
-        .then(res => res.json())
-        .then((data)=>{
-            console.log(data)
-            this.setState({posts : data})
-           
-        })
-  
-    }
-  seePost(id){
-    window.location.href = '/#/post/'+id;
+    this.getPosts = this.getPosts.bind(this)
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  getPosts() {
+    fetch("http://51.255.175.118:2000/post", {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data)
+        this.setState({ posts: data, 
+          list: data })
+
+      })
+
+  }
+  seePost(id) {
+    window.location.href = '/#/post/' + id;
+  }
+
+  handleChange(e) {
+    let currentList = this.state.list
+    let newList = [];
+    // If the search bar isn't empty
+    if (e.target.value !== "") {
+      this.setState({
+        posts: currentList
+      })
+
+      // Use .filter() to determine which items should be displayed
+      // based on the search terms
+      newList = currentList.filter(item => {
+        const lc = item.title.toLowerCase();
+        const filter = e.target.value.toLowerCase();
+        return lc.includes(filter)
+      });
+      this.setState({
+        posts: newList
+      })
+    } else {
+      this.setState({
+        posts: this.state.list
+      })
+    }
+    this.setState({
+      filtered: newList
+    })
+  }
+
   render() {
     return (
-     <div>
-       {this.state.posts != null ? 
-            this.state.posts.map((val)=>
-            <div class="card" onClick={()=>{this.seePost(val.post_id)}}>
-          
-          
-            <div class="card-content">
-              <div class="media">
-                <div class="media-left">
-                  <figure class="image is-48x48">
-                    <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
-                  </figure>
-                </div>
-                <div class="media-content">
-                  <p class="title is-4">{val.title}</p>
-                  <p class="subtitle is-6">{val.username}</p>
-                </div>
-              </div>
-          
-              <div class="content">
-                {val.description}
-               
-                <br/>
-                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-              </div>
-            </div>
-          </div>
-            
-                
-        )
-        :
-        null
-        }
-       
-</div>
+      <div class="columns ">
+        <div class="column is-7-desktop is-full-mobile is-offset-1">
+          {this.state.posts != null ?
+            this.state.posts.map((val) =>
+              <div class="card" onClick={() => { this.seePost(val.post_id) }}>
+                <div class="card-content">
+                  <div key="{va.post_id}">
+                    {(val.url_image != "") && (val.url_image != null) ?
+                      <div class="media-left">
+                        <figure class="image is-48x48">
+                          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                        </figure>
+                      </div> : null}
 
+                    <div class="media-content">
+                      <div class="post_title">
+                        <h4 class="title is-4" id="post_title">{val.title}</h4>
+                        <h4 class="title is-4" id="post_id">#{val.post_id}</h4>
+                      </div>
+                      <div class="description">
+                        <p>{val.description}</p>
+                      </div>
+                      <div class="infos">
+                        <Moment fromNow>
+                          {val.date}
+                        </Moment>
+                        <p class="author">{val.username}</p>
+                      </div>
+                    </div>
+                    <div class="rating">
+                      <img src="ear.png" class="icon"></img>
+                      <img src="comment.png" class="icon"></img>
+                      <img src="warning.png" class="icon"></img>
+                    </div>
+                    <div class="bestanswer">
+                      <p>Best answer</p>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            )
+            :
+            null
+          }
+
+        </div>
+        <div class="column is-3-desktop is-desktop">
+          <h3>Filter</h3>
+          <input type="text" onChange={this.handleChange} className="input" placeholder="Search..." />
+        </div>
+      </div>
 
     );
   }
 }
- 
+
 export default Posts;
