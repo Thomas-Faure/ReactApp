010ca405bd81d14d7c301cfb0e4a436456a5c155
flo4313 <fsmague@gmail.com>
2020-03-24 21:23:09 +0100

    Update account

---
+++ src/components/Account.js
@@ -0,0 +1,218 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { login, setUser, unsetPopUp, setPopUp } from '../actions';
+import sha256 from 'sha256';
+import { FormattedMessage, injectIntl } from 'react-intl';
+class Account extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      valueUsername: "",
+      valueFirstname: "",
+      valueLastname: "",
+      valueMail: "",
+      valueSexe: "",
+      valueBirthday: "",
+      valuePassword: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChangeFirstname = this.handleChangeFirstname.bind(this)
+    this.handleChangeUsername = this.handleChangeUsername.bind(this)
+    this.handleChangeLastname = this.handleChangeLastname.bind(this)
+    this.handleChangeSexe = this.handleChangeSexe.bind(this)
+    this.handleChangeMail = this.handleChangeMail.bind(this)
+    this.handleChangeBirthday = this.handleChangeBirthday.bind(this)
+    this.handleChangePassword = this.handleChangePassword.bind(this)
+    this.getData = this.getData.bind(this)
+    this.sendData = this.sendData.bind(this)
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  handleChangeUsername(event) {
+    this.setState({ valueUsername: event.target.value })
+  }
+  handleChangeFirstname(event) {
+    this.setState({ valueFirstname: event.target.value })
+  }
+  handleChangeLastname(event) {
+    this.setState({ valueLastname: event.target.value })
+  }
+  handleChangeSexe(event) {
+    this.setState({ valueSexe: event.target.value })
+  }
+
+
+  handleChangeMail(event) {
+    this.setState({ valueMail: event.target.value })
+  }
+  handleChangeBirthday(event) {
+    this.setState({ valueBirthday: event.target.value })
+  }
+  handleChangePassword(event) {
+    this.setState({ valuePassword: event.target.value })
+  }
+
+  handleSubmit(event) {
+    this.sendData()
+    event.preventDefault();
+  }
+
+  getData() {
+    var user = this.props.user
+    this.setState({
+      valueUsername: user.username,
+      valueFirstname: user.firstname,
+      valueMail: user.mail,
+      valueLastname: user.lastname,
+      valueSexe: user.sexe,
+      valueBirthday: user.birthday.slice(0, 10)
+    })
+  }
+
+  async sendData() {
+    console.log(this.state.birthday)
+    const token = localStorage.token;
+    fetch("https://thomasfaure.fr/user/edit", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      },
+      body: JSON.stringify({
+        username: this.state.valueUsername,
+        firstname: this.state.valueFirstname,
+        lastname: this.state.valueLastname,
+        birthday: this.state.valueBirthday,
+        mail: this.state.valueMail,
+        sexe: this.state.valueSexe,
+        admin: this.props.user.admin,
+        password: (this.state.valuePassword.length > 1 ? sha256(this.state.valuePassword) : "")
+      })
+    })
+      .then(res => res.json())
+      .then((data) => {
+        if (data.affectedRows === 1) {
+          let asyncUpdate = async () => {
+            if (token !== undefined) {
+              var user_id = this.props.user.user_id
+
+              var info = await fetch("https://thomasfaure.fr/user/" + user_id, {
+                method: "GET",
+                headers: {
+                  'Authorization': 'Bearer ' + token
+                }
+              })
+              info = await info.json()
+              this.props.setUser(info[0])
+              this.props.unsetPopUp()
+
+            } else {
+              this.setState({ errorLogin: true })
+            }
+          }
+          asyncUpdate()
+
+        }
+
+      })
+  }
+
+  render() {
+    const { formatMessage } = this.props.intl;
+
+    return (
+      <section className=" is-fullheight" style={{ backgroundColor: "#BBDCF2", borderRadius: "5px" }}>
+        <div className="">
+          <div className="container has-text-centered">
+            <div className="column">
+              <h3 className="title has-text-black"><FormattedMessage id="account.label" /></h3>
+              <hr className="login-hr" />
+              <p className="subtitle has-text-black"><FormattedMessage id="account.sentence" /></p>
+              <div className="box">
+                <form onSubmit={this.handleSubmit}>
+                  <div className="field">
+                    <label className="label">Username</label>
+                    <div className="control">
+                      <input className="input" type="text" placeholder="Username" value={this.state.valueUsername} onChange={this.handleChangeUsername} />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Firstname</label>
+                    <div className="control">
+                      <input className="input" type="text" placeholder="Firstname" value={this.state.valueFirstname} onChange={this.handleChangeFirstname} />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">LastName</label>
+                    <div className="control">
+                      <input className="input" type="text" placeholder="Lastname" value={this.state.valueLastname} onChange={this.handleChangeLastname} />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Password <strong style={{ color: "red" }}>empty to avoid modification</strong></label>
+                    <div className="control">
+                      <input className="input" type="password" placeholder="Password" value={this.state.valuePassword} onChange={this.handleChangePassword} />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Mail</label>
+                    <div className="control">
+                      <input className="input" type="text" placeholder="Mail" value={this.state.valueMail} onChange={this.handleChangeMail} />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Birthday</label>
+                    <div className="control">
+
+                      <input className="input" type="date" placeholder="Birthday" value={this.state.valueBirthday} onChange={this.handleChangeBirthday} />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Sexe</label>
+                    <div className="control">
+                      <div className="select" style={{ width: "100%" }}>
+                        <select value={this.state.valueSexe} onChange={this.handleChangeSexe}>
+                          <option value="M">Man</option>
+                          <option value="F">Woman</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="control">
+                    <input className="button is-link" type="submit" value="submit"></input>
+
+                  </div>
+
+
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </section>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+    setUser,
+    unsetPopUp,
+    setPopUp
+
+  }
+}
+
+export default injectIntl(connect(mapStateToProps, mapDispatchToProps())(Account));
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import fetchPosts from '../fetch/fetchPosts'
 import Login from './Login'
 import Register from './Register'
 import fetchUsers from '../fetch/fetchUsers'
-import {FormattedMessage} from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 
 import {
@@ -35,7 +35,7 @@ import BackOfficeEditCommentCategory from './backoffice/edit/CommentCategory'
 import BackOfficeShowReportComments from './backoffice/show/ReportComment'
 import BackOfficeShowReportPosts from './backoffice/show/ReportPost'
 import NewPasswordForm from './NewPasswordForm'
-import { login, logoff, setUser, unSetUser, setPopUp, unsetPopUp,changeLanguage } from '../actions';
+import { login, logoff, setUser, unSetUser, setPopUp, unsetPopUp, changeLanguage } from '../actions';
 import sha256 from 'sha256';
 import fetchBestAnswer from "../fetch/fetchBestAnswer";
 import fetchCommentCategories from "../fetch/fetchCommentCategories";
@@ -49,7 +49,7 @@ class Main extends Component {
     super(props)
 
     this.state = {
-     
+
       burgerOpen: false
     }
 
@@ -107,14 +107,14 @@ class Main extends Component {
 
     await this.props.fetchPostCategories()
     await this.props.fetchCommentCategories()
-     this.props.fetchPosts()
-     this.props.fetchBestAnswer()
-    
+    this.props.fetchPosts()
+    this.props.fetchBestAnswer()
+
 
   }
 
-  clickBurger(){
-    this.setState({burgerOpen: (!this.state.burgerOpen)})
+  clickBurger() {
+    this.setState({ burgerOpen: (!this.state.burgerOpen) })
   }
   logoff() {
     this.props.logoff();
@@ -125,171 +125,183 @@ class Main extends Component {
 
   render() {
     return (
-     
-        <div>
-          <HashRouter>
-            {this.props.popUp.page != "addPost" ? null
-              :
-              <AddPost></AddPost>
-            }
 
-            {this.props.popUp.page != "postDetails" ? null
-              :
-              <PostDetails></PostDetails>
-            }
+      <div>
+        <HashRouter>
+          {this.props.popUp.page != "addPost" ? null
+            :
+            <AddPost></AddPost>
+          }
 
-            {!this.props.isLogged ?
-              <div>
-                {this.props.popUp.page != "register" ? null :
-                  <div className={'modal is-active'}>
-                    <div className="modal-background" onClick={() => { this.props.unsetPopUp() }}></div>
-                    <div className="modal-card">
-                      <header className="modal-card-head">
-                        <p className="modal-card-title">Register</p>
-                        <button className="delete" aria-label="close" onClick={() => { this.props.unsetPopUp() }}></button>
-                      </header>
-                      <section className="modal-card-body">
-                        <Register></Register>
-                      </section>
-                      <footer className="modal-card-foot">
-                        <button className="button" onClick={() => { this.props.unsetPopUp() }}>Cancel</button>
-                      </footer>
-                    </div>
+          {this.props.popUp.page != "postDetails" ? null
+            :
+            <PostDetails></PostDetails>
+          }
+
+          {!this.props.isLogged ?
+            <div>
+              {this.props.popUp.page != "register" ? null :
+                <div className={'modal is-active'}>
+                  <div className="modal-background" onClick={() => { this.props.unsetPopUp() }}></div>
+                  <div className="modal-card">
+                    <header className="modal-card-head">
+                      <p className="modal-card-title">Register</p>
+                      <button className="delete" aria-label="close" onClick={() => { this.props.unsetPopUp() }}></button>
+                    </header>
+                    <section className="modal-card-body">
+                      <Register></Register>
+                    </section>
+                    <footer className="modal-card-foot">
+                      <button className="button" onClick={() => { this.props.unsetPopUp() }}>Cancel</button>
+                    </footer>
                   </div>
-                  }
-                {this.props.popUp.page != "login" ? null :
-                  <div className={'modal is-active'}>
-                    <div className="modal-background" onClick={() => { this.props.unsetPopUp() }}></div>
-                    <div className="modal-card">
-                      <header className="modal-card-head">
-                        <p className="modal-card-title">Login</p>
-                        <button className="delete" aria-label="close" onClick={() => { this.props.unsetPopUp() }}></button>
-                      </header>
-                      <section className="modal-card-body">
-                        <Login></Login>
-                      </section>
-                      <footer className="modal-card-foot">
-                        <button className="button" onClick={() => { this.props.unsetPopUp() }}>Cancel</button>
-                      </footer>
-                    </div>
-                  </div>}
-              </div>
-              : null}
-              {this.props.popUp.page != "account" ? null :
-                <Account></Account> 
+                </div>
               }
-            <nav className="navbar" style={{ backgroundColor: '#BBDCF2' }} role="navigation" aria-label="main navigation">
-              <div className="navbar-brand navbar-start">
-                <a className="navbar-item" href="/#/">
+              {this.props.popUp.page != "login" ? null :
+                <div className={'modal is-active'}>
+                  <div className="modal-background" onClick={() => { this.props.unsetPopUp() }}></div>
+                  <div className="modal-card">
+                    <header className="modal-card-head">
+                      <p className="modal-card-title">Login</p>
+                      <button className="delete" aria-label="close" onClick={() => { this.props.unsetPopUp() }}></button>
+                    </header>
+                    <section className="modal-card-body">
+                      <Login></Login>
+                    </section>
+                    <footer className="modal-card-foot">
+                      <button className="button" onClick={() => { this.props.unsetPopUp() }}>Cancel</button>
+                    </footer>
+                  </div>
+                </div>}
+            </div>
+            : null}
+          {this.props.popUp.page != "account" ? null :
+            <div className={'modal is-active'}>
+              <div className="modal-background" onClick={() => { this.props.unsetPopUp() }}></div>
+              <div className="modal-card">
+                <header className="modal-card-head">
+                  <p className="modal-card-title">Account</p>
+                  <button className="delete" aria-label="close" onClick={() => { this.props.unsetPopUp() }}></button>
+                </header>
+                <section className="modal-card-body">
+                  <Account></Account>
+                </section>
+                <footer className="modal-card-foot">
+                </footer>
+              </div>
+            </div>}
+        <nav className="navbar" style={{ backgroundColor: '#BBDCF2' }} role="navigation" aria-label="main navigation">
+          <div className="navbar-brand navbar-start">
+            <a className="navbar-item" href="/#/">
 
-                  <img src="logo1.png" alt="logo" />
+              <img src="logo1.png" alt="logo" />
 
-                </a>
+            </a>
 
-                {this.state.burgerOpen != true ? 
-                <a role="button" className="navbar-burger burger"  onClick={()=>this.clickBurger()} aria-expanded="false" data-target="navbarBasicExample">
+            {this.state.burgerOpen != true ?
+              <a role="button" className="navbar-burger burger" onClick={() => this.clickBurger()} aria-expanded="false" data-target="navbarBasicExample">
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
               </a>
-                
-                :
-                <a role="button" className="navbar-burger burger is-active"  onClick={()=>this.clickBurger()} aria-expanded="false" data-target="navbarBasicExample">
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
+
+              :
+              <a role="button" className="navbar-burger burger is-active" onClick={() => this.clickBurger()} aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+
+
+            }
+
+          </div>
+
+          <div id="navbarBasicExample" className={this.state.burgerOpen != true ? "navbar-menu" : "navbar-menu is-active"}>
+            <div className="navbar-start">
+              <a className="navbar-item" onClick={() => { this.setState({ burgerOpen: false }); window.location.href = '/#/' }}>
+                <FormattedMessage id="navbar.home" />
+              </a>
+              <a className="navbar-item" onClick={() => { this.setState({ burgerOpen: false }); window.location.href = '/#/contact' }}>
+                <FormattedMessage id="navbar.contact" />
+              </a>
+              <a className="navbar-item" onClick={() => { this.setState({ burgerOpen: false }); window.location.href = '/#/informations' }}>
+                <FormattedMessage id="navbar.informations" />
+              </a>
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">
+                  <FormattedMessage id="navbar.language" />
                 </a>
 
-                
-                }
-                
-              </div>
-              
-              <div id="navbarBasicExample"className={this.state.burgerOpen != true ?"navbar-menu" : "navbar-menu is-active" }>
-                <div className="navbar-start">
-                  <a className="navbar-item" onClick={()=>{this.setState({burgerOpen: false});window.location.href='/#/'}}>
-                  <FormattedMessage id="navbar.home"/>
-      </a>
-                  <a className="navbar-item" onClick={()=>{this.setState({burgerOpen: false});window.location.href='/#/contact'}}>
-                  <FormattedMessage id="navbar.contact"/>
-      </a>
-                  <a className="navbar-item" onClick={()=>{this.setState({burgerOpen: false});window.location.href='/#/informations'}}>
-                  <FormattedMessage id="navbar.informations"/>
-      </a>
-      <div className="navbar-item has-dropdown is-hoverable">
-        <a className="navbar-link">
-        <FormattedMessage id="navbar.language"/>
-        </a>
-
-        <div className="navbar-dropdown">
-          <a className="navbar-item" onClick={()=>{this.props.changeLanguage("fr")}}>
-            Fr
+                <div className="navbar-dropdown">
+                  <a className="navbar-item" onClick={() => { this.props.changeLanguage("fr") }}>
+                    Fr
           </a>
-          <a className="navbar-item" onClick={()=>{this.props.changeLanguage("en")}}>
-            En
+                  <a className="navbar-item" onClick={() => { this.props.changeLanguage("en") }}>
+                    En
           </a>
 
-        </div>
-      </div>
+                </div>
+              </div>
 
-                  {this.props.user != null ?
-                    (this.props.user.admin == 1) ?
-                      <a className="navbar-item" onClick={()=>{this.setState({burgerOpen: false});window.location.href='/#/backoffice'}}>
-                        <FormattedMessage id="navbar.backoffice"/>
-      </a>
-                      :
-                      null
-                    :
-                    null
-                  }
-                </div>
-                <div className="navbar-end">
-                  <div className="navbar-item">
-                    <div>
-                      {!this.props.isLogged ? <a onClick={() => { this.setState({burgerOpen: false});this.props.setPopUp("login", null) }} className="navbar-item">
-                        Login
-                  </a> : <div><a className="navbar-item" onClick={() => {this.setState({burgerOpen: false});this.props.setPopUp("acount", null)}}>Account</a>
-                  <a className="navbar-item" onClick={() => {this.setState({burgerOpen: false});this.logoff()}}>Logout</a></div>}
-                    </div>
-                  </div>
+              {this.props.user != null ?
+                (this.props.user.admin == 1) ?
+                  <a className="navbar-item" onClick={() => { this.setState({ burgerOpen: false }); window.location.href = '/#/backoffice' }}>
+                    <FormattedMessage id="navbar.backoffice" />
+                  </a>
+                  :
+                  null
+                :
+                null
+              }
+            </div>
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div>
+                  {!this.props.isLogged ? <a onClick={() => { this.setState({ burgerOpen: false }); this.props.setPopUp("login", null) }} className="navbar-item">
+                    Login
+                  </a> : <div className="flex"><a className="navbar-item" onClick={() => { this.setState({ burgerOpen: false }); this.props.setPopUp("account", null) }}><FormattedMessage id="account.label" /></a>
+                      <a className="navbar-item" onClick={() => { this.setState({ burgerOpen: false }); this.logoff() }}>Logout</a></div>}
                 </div>
               </div>
-            </nav>
-            <section className="section">
-            
-              <div className="container">
-                <Route exact path="/" component={PostsList} />
-                <Route path="/posts" component={PostsList} />
-                <Route path="/post/:id" component={PostDetails} />
-                <Route path="/login" component={Login} />
-                <Route path="/account" component={account} />
-                <Route path="/register" component={Register} />
-                <Route exact path="/addPost" component={AddPost} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/informations" component={Informations} />
-                <Route exact path="/backoffice" component={BackOfficeIndex} />
-                <Route exact path="/backoffice/users" component={BackOfficeShowUsers} />
-                <Route exact path="/backoffice/posts" component={BackOfficeShowPosts} />
-                <Route exact path="/backoffice/postCategories" component={BackOfficeShowPostCategories} />
-                <Route exact path="/backoffice/commentCategories" component={BackOfficeShowCommentCategories} />
-                <Route exact path="/backoffice/posts/create" component={BackOfficeCreatePost} />
-                <Route exact path="/backoffice/users/create" component={BackOfficeCreateUser} />
-                <Route exact path="/backoffice/postCategories/create" component={BackOfficeCreatePostCategory} />
-                <Route exact path="/backoffice/commentCategories/create" component={BackOfficeCreateCommentCategory} />
-                <Route exact path="/backoffice/users/:id/edit" component={BackOfficeEditUser} />
-                <Route exact path="/backoffice/posts/:id/edit" component={BackOfficeEditPost} />
-                <Route exact path="/backoffice/postCategories/:id/edit" component={BackOfficeEditPostCategory} />
-                <Route exact path="/backoffice/commentCategories/:id/edit" component={BackOfficeEditCommentCategory} />
-                <Route exact path="/backoffice/posts/:id/comments" component={BackOfficeShowComments} />
-                <Route exact path="/backoffice/posts/:post_id/comments/:comment_id/edit" component={BackOfficeEditComment} />
-                <Route exact path="/backoffice/reportComments" component={BackOfficeShowReportComments} />
-                <Route exact path="/backoffice/reportPosts" component={BackOfficeShowReportPosts} />
-                <Route exact path="/forgotPassword/:token" component={NewPasswordForm} />
-              </div>
-            </section>
+            </div>
+          </div>
+        </nav>
+        <section className="section">
+
+          <div className="container">
+            <Route exact path="/" component={PostsList} />
+            <Route path="/posts" component={PostsList} />
+            <Route path="/post/:id" component={PostDetails} />
+            <Route path="/login" component={Login} />
+            <Route path="/account" component={Account} />
+            <Route path="/register" component={Register} />
+            <Route exact path="/addPost" component={AddPost} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/informations" component={Informations} />
+            <Route exact path="/backoffice" component={BackOfficeIndex} />
+            <Route exact path="/backoffice/users" component={BackOfficeShowUsers} />
+            <Route exact path="/backoffice/posts" component={BackOfficeShowPosts} />
+            <Route exact path="/backoffice/postCategories" component={BackOfficeShowPostCategories} />
+            <Route exact path="/backoffice/commentCategories" component={BackOfficeShowCommentCategories} />
+            <Route exact path="/backoffice/posts/create" component={BackOfficeCreatePost} />
+            <Route exact path="/backoffice/users/create" component={BackOfficeCreateUser} />
+            <Route exact path="/backoffice/postCategories/create" component={BackOfficeCreatePostCategory} />
+            <Route exact path="/backoffice/commentCategories/create" component={BackOfficeCreateCommentCategory} />
+            <Route exact path="/backoffice/users/:id/edit" component={BackOfficeEditUser} />
+            <Route exact path="/backoffice/posts/:id/edit" component={BackOfficeEditPost} />
+            <Route exact path="/backoffice/postCategories/:id/edit" component={BackOfficeEditPostCategory} />
+            <Route exact path="/backoffice/commentCategories/:id/edit" component={BackOfficeEditCommentCategory} />
+            <Route exact path="/backoffice/posts/:id/comments" component={BackOfficeShowComments} />
+            <Route exact path="/backoffice/posts/:post_id/comments/:comment_id/edit" component={BackOfficeEditComment} />
+            <Route exact path="/backoffice/reportComments" component={BackOfficeShowReportComments} />
+            <Route exact path="/backoffice/reportPosts" component={BackOfficeShowReportPosts} />
+            <Route exact path="/forgotPassword/:token" component={NewPasswordForm} />
+          </div>
+        </section>
 
           </HashRouter>
-        </div>
+        </div >
 
     );
   }
@@ -303,7 +315,7 @@ const mapStateToProps = (state) => {
     comment: state.comment,
     user: state.user,
     popUp: state.popUp,
-    bestAnswer:state.bestAnswer
+    bestAnswer: state.bestAnswer
   }
 }
 
@@ -319,7 +331,7 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   fetchPostCategories: fetchPostCategories,
   fetchUsers: fetchUsers,
   fetchBestAnswer: fetchBestAnswer,
-  changeLanguage:changeLanguage
+  changeLanguage: changeLanguage
 
 }, dispatch)
 
